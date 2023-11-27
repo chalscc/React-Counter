f3f5bfaac6b123aa63485a6a703dcd41a209f2dd
chalscc <carlescatalancalabuig@gmail.com>
2023-11-27 16:47:29 +0100

    HandlerReset y HandlerSubstract eventos

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,30 +1,43 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 
 
-function CounterApp({value}) {    
+function CounterApp({value}) {
     
+    const [counter, setCounter] = useState(value);
+
     const handleAdd = () => {
-        console.log('+1');
+        setCounter(counter + 1);
+    }
+
+    const handleReset = () => {
+        setCounter(value);
+    }
+
+    const handleSubstract = () => {
+        setCounter(counter -1);
     }
     
     return (
     <>
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
 
         <button onClick={ handleAdd }>
             +1
+        </button>
+        <button onClick={ handleSubstract }>
+            -1
+        </button>
+        <button onClick={ handleReset }>
+            Reset
         </button>
     </>
     );    
 }
 
-CounterApp.PropTypes = {
+CounterApp.propTypes = {
     value: PropTypes.number.isRequired
-}
-CounterApp.defaultProps = {
-    value: 0
 }
 
 export default CounterApp;
