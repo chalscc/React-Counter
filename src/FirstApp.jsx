40822c93fe359ab8f5fc79c3f6e479fd2e9b0cd6
chalscc <carlescatalancalabuig@gmail.com>
2023-11-27 13:14:50 +0100

    Comunicacion entre componentes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,11 +1,12 @@
 const newMessage = 123
 
 
-const FirstApp = () => {
+const FirstApp = ( {title = 'Default', suma} ) => {
 
    return (
     <>
-        <h1>{ newMessage }</h1>
+        <h1>{ title }</h1>
+        <h1>{ suma }</h1>
         <p>React is a JavaScript library for building user interfaces.</p>
     </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp />
-        <FirstApp />
+        <FirstApp title='Hola' suma = {1+1} />
     </React.StrictMode>
 )
