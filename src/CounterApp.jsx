dfac7160dca4812be461eb86e88a144b1ff3e5ad
chalscc <carlescatalancalabuig@gmail.com>
2023-11-27 16:28:17 +0100

    Evento click

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,13 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function CounterApp({value}) {
+
+function CounterApp({value}) {    
     
+    const handleAdd = () => {
+        console.log('+1');
+    }
     
     return (
     <>
         <h1>CounterApp</h1>
         <h2> { value } </h2>
+
+        <button onClick={ handleAdd }>
+            +1
+        </button>
     </>
     );    
 }
