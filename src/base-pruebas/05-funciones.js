c2c816181f27e1e7fb47954e1e2f74a4ca97be1e
chalscc <carlescatalancalabuig@gmail.com>
2023-11-27 17:18:28 +0100

    Bases para realizar test unitarios

---
+++ src/base-pruebas/05-funciones.js
@@ -0,0 +1,40 @@
+
+// Funciones en JS
+// const saludar = function( nombre ) {
+//     return `Hola, ${ nombre }`;
+// }
+
+const saludar2 = ( nombre ) => {
+    return `Hola, ${ nombre }`;
+}
+
+const saludar3 = ( nombre ) => `Hola, ${ nombre }`;
+const saludar4 = () => `Hola Mundo`;
+
+// console.log( saludar('Goku') )
+
+console.log( saludar2('Pepito') );
+console.log( saludar3('Juanito') );
+console.log( saludar4() );
+
+
+const getUser = () => ({
+        uid: 'ABC123',
+        username: 'Mi_Username'
+});
+
+
+const user = getUser();
+console.log(user);
+
+// Tarea
+const getUsuarioActivo = ( nombre ) =>({
+    uid: 'ABC567',
+    username: nombre
+})
+
+const usuarioActivo = getUsuarioActivo('Carles');
+console.log( usuarioActivo );
+
+
+
