3655be017f7904b5d41ced3355e625c9d961ed94
chalscc <carlescatalancalabuig@gmail.com>
2023-11-28 13:02:22 +0100

    Configuración de Jest con Babel y realización de test simple

--- src/base-pruebas/02-template-string.js
+++ src/base-pruebas/02-template-string.js
@@ -1,18 +1,3 @@
-
-
-
-const nombre   = 'Carles';
-const apellido = 'Catalan';
-
-
-// const nombreCompleto = nombre + ' ' + apellido;
-const nombreCompleto = `${ nombre } ${ apellido }`;
-
-console.log( nombreCompleto );
-
-
-function getSaludo(nombre) {
+export function getSaludo(nombre) {
     return 'Hola ' + nombre;
 }
-
-console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
