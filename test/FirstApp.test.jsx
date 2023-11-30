8ae77355e5e9fd7e017f3d963ca28da40a679267
chalscc <carlescatalancalabuig@gmail.com>
2023-11-30 15:32:50 +0100

    Creación de snapshots sobre componentes jsx

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -6,7 +6,23 @@ describe('Pruebas en <FirstApp /> component', () => {
 
         const title = 'Hola, soy Carles'
 
-        render(<FirstApp title={ title } />)
+        const { container } = render(<FirstApp title={ title } />)
 
+        // console.log(container)
+
+        expect(container).toMatchSnapshot()
      })
+
+     test('debe de mostrar el titulo en un h1', () => { 
+
+        const title = 'Hola, soy Carles'
+
+        const { container, getByText } = render(<FirstApp title={ title } />)
+
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+
+        expect(h1.innerHTML).toContain(title)
+    })
  })
